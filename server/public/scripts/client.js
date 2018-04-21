421430cec51ae594961400a6a93a14d031b59470
Cory Booth <Rodimus2005@hotmail.com>
2018-04-21 14:14:38 -0500

    base mode is complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,15 +4,15 @@ console.log('JAVA JIVE');
 $(document).ready(onReady);
 
 
-
 function onReady() {
     console.log('jQuery is go for launch');
     $('#add').on('click', newAdd);
-  //  $('#sub').on('click', newSub);
-    //$('#multiply').on('click', newMulti);
-   // $('#divide').on('click', newDivide);
+    $('#sub').on('click', newSub);
+    $('#multiply').on('click', newMulti);
+    $('#divide').on('click', newDivide);
 }
 
+//takes input "add" numbers from the DOM and sends them to the server to be added.
 function newAdd (){
     const newProblem = {
     x : $('#numOne').val(),
@@ -31,6 +31,64 @@ function newAdd (){
     });
 }
 
+//takes input "subtract" numbers from the DOM and sends them to the server to be subtracted.
+function newSub (){
+    const newProblem = {
+    x : $('#numOne').val(),
+    y: $('#numTwo').val(),
+    type: "Subtract"
+    };
+    console.log('New problem object', newProblem);
+    $.ajax({
+        method: 'POST',
+        url: '/sub-numbers',
+        data: newProblem
+    })
+    .then(function(response){
+        console.log(response);
+        logAllProblems();
+    });
+}
+
+//takes input "multi" numbers from the DOM and sends them to the server to be multiplied.
+function newMulti (){
+    const newProblem = {
+    x : $('#numOne').val(),
+    y: $('#numTwo').val(),
+    type: "Multi"
+    };
+    console.log('New problem object', newProblem);
+    $.ajax({
+        method: 'POST',
+        url: '/multi-numbers',
+        data: newProblem
+    })
+    .then(function(response){
+        console.log(response);
+        logAllProblems();
+    });
+}
+
+//takes input "divide" numbers from the DOM and sends them to the server to be divided.
+function newDivide (){
+    const newProblem = {
+    x : $('#numOne').val(),
+    y: $('#numTwo').val(),
+    type: "Divide"
+    };
+    console.log('New problem object', newProblem);
+    $.ajax({
+        method: 'POST',
+        url: '/divide-numbers',
+        data: newProblem
+    })
+    .then(function(response){
+        console.log(response);
+        logAllProblems();
+    });
+}
+
+//get the history of all problems from the server and append them to the DOM.
 function logAllProblems (){
     $.ajax({
         type: 'GET', 
@@ -38,9 +96,11 @@ function logAllProblems (){
     })
     .then(function(response) {
         $('#allProblems').empty();
+        $('#history').empty();
+        $('#history').append('History:')
         response.forEach(function(element){
         $('#allProblems').append(
-            '<li>' + element.x + element.type + element.y + element.ans+ '<li>')
+            '<li>' + element.x + ' ' + element.type + ' ' + element.y + ' = ' + element.ans + '</li>')
         });
     });
 }
--- server/server.js
+++ server/server.js
@@ -32,6 +32,53 @@ app.post('/add-numbers', (req, res) => {
     res.sendStatus(200);
 });
 
+//subtracts received numbers and pushes them to the history array.
+app.post('/sub-numbers', (req, res) => {
+    console.log(req.body);
+      let subbedNum = (parseInt((req.body).x) - parseInt((req.body).y));
+      console.log(subbedNum);
+    const newHistory = {
+         x : (req.body).x,
+        type : "-",
+        y:  (req.body).y,
+        ans : subbedNum
+        };
+    history.push(newHistory);
+    console.log(history);
+    res.sendStatus(200);
+});
+
+//multipies received numbers and pushes them to the history array.
+app.post('/multi-numbers', (req, res) => {
+    console.log(req.body);
+      let multiNum = (parseInt((req.body).x) * parseInt((req.body).y));
+      console.log(multiNum);
+    const newHistory = {
+         x : (req.body).x,
+        type : "*",
+        y:  (req.body).y,
+        ans : multiNum
+        };
+    history.push(newHistory);
+    console.log(history);
+    res.sendStatus(200);
+});
+
+//divdes received numbers and pushes them to the history array.
+app.post('/divide-numbers', (req, res) => {
+    console.log(req.body);
+      let divideNum = (parseInt((req.body).x) / parseInt((req.body).y));
+      console.log(divideNum);
+    const newHistory = {
+         x : (req.body).x,
+        type : "/",
+        y:  (req.body).y,
+        ans : divideNum
+        };
+    history.push(newHistory);
+    console.log(history);
+    res.sendStatus(200);
+});
 
 //function returnAdd (){
  //   (addNumbers[0].x + addNumbers[0].y) = addedNums;
